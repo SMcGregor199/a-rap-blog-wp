973f3ddea4cbf16089abdd885de92e9e9849ade4
SMcGregor199 <shaynemcgregor1@gmail.com>
2024-03-15 10:30:30 -0400

    …

--- wp-content/themes/a-rap-blog/front-page.php
+++ wp-content/themes/a-rap-blog/front-page.php
@@ -4,9 +4,11 @@ ini_set('display_errors','1');
 error_reporting(E_ALL);
 echo '<h1>This site is currently under construction</h1>';
 $rawData = file_get_contents('php://input');
-if($rawData){
-	echo "Client Name: " . $rawData['client_name'] . "<br>";
-   echo "Client Address: " . $rawData['client_address'];
-} else {
-    echo "No data received";
-  }
+
+echo $rawData;
+// if($rawData){
+// 	echo "Client Name: " . $rawData['client_name'] . "<br>";
+//    echo "Client Address: " . $rawData['client_address'];
+// } else {
+//     echo "No data received";
+//   }
