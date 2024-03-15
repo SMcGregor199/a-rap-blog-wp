e5b8572d51bc6d66c34ad36568b2e273d9b254ad
SMcGregor199 <shaynemcgregor1@gmail.com>
2024-03-15 10:35:04 -0400

    …

--- wp-content/themes/a-rap-blog/front-page.php
+++ wp-content/themes/a-rap-blog/front-page.php
@@ -4,9 +4,3 @@ ini_set('display_errors','1');
 error_reporting(E_ALL);
 echo '<h1>This site is currently under construction</h1>';
 // Access data from POST request
-$clientName = $_POST['client_name'];
-$clientAddress = $_POST['client_address'];
-
-// Process the data (e.g., display or store)
-echo "Client Name: " . $clientName . "<br>";
-echo "Client Address: " . $clientAddress;
